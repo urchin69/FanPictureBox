32603cf36dfc4fd7ef8b99a881e0dfae443a914e
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-10-02 22:48:16 +0200

    Add project files.

---
+++ FanPictureBox/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace FanPictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAddPicture = new System.Windows.Forms.Button();
+            this.btnDelPicture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictBox1
+            // 
+            this.pictBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictBox1.Image")));
+            this.pictBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictBox1.MaximumSize = new System.Drawing.Size(800, 364);
+            this.pictBox1.MinimumSize = new System.Drawing.Size(800, 364);
+            this.pictBox1.Name = "pictBox1";
+            this.pictBox1.Size = new System.Drawing.Size(800, 364);
+            this.pictBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictBox1.TabIndex = 0;
+            this.pictBox1.TabStop = false;
+            this.pictBox1.WaitOnLoad = true;
+            // 
+            // btnAddPicture
+            // 
+            this.btnAddPicture.Location = new System.Drawing.Point(44, 398);
+            this.btnAddPicture.Name = "btnAddPicture";
+            this.btnAddPicture.Size = new System.Drawing.Size(147, 39);
+            this.btnAddPicture.TabIndex = 1;
+            this.btnAddPicture.Text = "Dodaj  zdjęcie";
+            this.btnAddPicture.UseVisualStyleBackColor = true;
+            this.btnAddPicture.Click += new System.EventHandler(this.btnAddPicture_Click);
+            // 
+            // btnDelPicture
+            // 
+            this.btnDelPicture.Location = new System.Drawing.Point(546, 398);
+            this.btnDelPicture.Name = "btnDelPicture";
+            this.btnDelPicture.Size = new System.Drawing.Size(174, 38);
+            this.btnDelPicture.TabIndex = 2;
+            this.btnDelPicture.Text = "Usuń zdjęcie";
+            this.btnDelPicture.UseVisualStyleBackColor = true;
+            this.btnDelPicture.Click += new System.EventHandler(this.btnDelPicture_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelPicture);
+            this.Controls.Add(this.btnAddPicture);
+            this.Controls.Add(this.pictBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictBox1;
+        private System.Windows.Forms.Button btnAddPicture;
+        private System.Windows.Forms.Button btnDelPicture;
+    }
+}
+
---
+++ FanPictureBox/Form1.cs
@@ -0,0 +1,122 @@
+﻿using FanPictureBox.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FanPictureBox
+{
+    public partial class Form1 : Form
+    {
+
+
+        public bool IsMaximize
+        {
+            get
+            {
+                return Settings.Default.IsMaximize;
+            }
+            set
+            {
+                Settings.Default.IsMaximize = value;
+
+            }
+
+        }
+    
+        public string PathPicture
+        {
+            get
+            {
+                return Settings.Default.PathPicture;
+            }
+            set
+            {
+                Settings.Default.PathPicture = value;
+            }
+
+        }
+
+
+
+
+
+
+
+        public Form1()//konstruktor
+        {
+            InitializeComponent();
+            if (IsMaximize)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+
+
+            if (PathPicture !="")
+            {
+                PathPicture = Settings.Default.PathPicture;
+
+            }
+
+
+
+
+
+
+        }
+
+
+        private void btnDelPicture_Click(object sender, EventArgs e)
+        {
+          pictBox1.Image = null;
+        }
+
+        private void btnAddPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pict = new OpenFileDialog();
+                if(pict.ShowDialog()==DialogResult.OK)
+            {
+                Bitmap bit = new Bitmap(pict.FileName);
+                pictBox1.Image = bit;
+                pictBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                IsMaximize = true;
+            else
+                IsMaximize = false;
+
+            Settings.Default.Save();
+
+            if (pictBox1.Image !=null)
+            {
+                PathPicture = pictBox1.Image.ToString();
+            }
+            else
+            {
+
+                Bitmap bit = null;
+                pictBox1.Image = bit;// PathPicture;
+            }
+
+
+
+        }
+    }
+
+}
+
